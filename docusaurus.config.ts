0e7200ece7c9643d20019c917c17324a5c1cac22
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2025-01-28 13:28:07 +0300

    chore: Docusaurus configuration for GitHub Pages deployment and localization

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,33 +2,19 @@ import { themes as prismThemes } from 'prism-react-renderer'
 import type { Config } from '@docusaurus/types'
 import type * as Preset from '@docusaurus/preset-classic'
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 const config: Config = {
   title: 'Welcome to tutorials and blogs',
   tagline: 'Tech Made Simple, Knowledge Made Powerful.',
   favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
-  url: '/blogs-and-tutorials/',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://burakboduroglu.github.io/',
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'burakboduroglu', // Usually your GitHub org/user name.
-  projectName: 'myTutorialsAndBlogs', // Usually your repo name.
-
+  organizationName: 'burakboduroglu',
+  projectName: 'blogs-and-tutorials',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['en', 'tr'],
   },
 
   presets: [
@@ -40,7 +26,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/burakboduroglu/myTutorialsAndBlogs/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
@@ -51,7 +37,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/burakboduroglu/myTutorialsAndBlogs/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -65,7 +51,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: 'img/header_icon.jpg',
     navbar: {
       title: 'Burak Boduroğlu |',
